7b8fc3f2c889c7aabd1b112f32653fbc679e306c
Duc Minh Le <ducleminh.91@gmail.com>
2012-03-01 02:09:47 +0700

    just a small change

--- Test Egit/src/HelloWorld.java
+++ Test Egit/src/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld
 	{
 		System.out.println("Hello, world!");
 		System.out.println("Add redundant");
-		System.out.println("Test EGit");
+		System.out.println("Test Differ");
 	}
 }
